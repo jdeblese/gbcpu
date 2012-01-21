f8487021953e8d4e9a0074c509e01b9e79a61ddc
Jan-willem De Bleser <jw@thescrapyard.org>
2012-01-22 00:43:03 +0100

    Untested rewrite of ALU

--- alu.vhd
+++ alu.vhd
@@ -13,24 +13,69 @@ use UNISIM.VComponents.all;
 --  RLC, RL, RRC, RR
 --  SLA, SRA, SRL, SWAP
 --  BIT, SET, RES
+--  DAA, CPL, SCF, CCF
 
--- Possible reduced opcode map:
---     000  001  010  011  100  101  110  111
--- 000 ADD  ADC  SUB  SBC  AND  XOR   OR   CP
--- 001 INC  DEC   -    -    -    -    -    -
--- 010 RLC   RL  RRC   RR  SLA  SRA  SRL  SWP
--- 011  -    -    -    -    -    -    -    -
--- 100 BIT0 BIT1 BIT2 BIT3 BIT4 BIT5 BIT6 BIT7
--- 101 SET0 SET1 SET2 SET3 SET4 SET5 SET6 SET7
+-- Reduced opcode map:
+--      000  001  010  011  100  101  110  111
+-- 000  ADD  ADC  SUB  SBC             CP
+-- 001  INC                           DEC
+-- 010  AND   OR  XOR  CPL
+-- 011  DAA       SCF  CCF
+-- 100  RLC   RL  RRC   RR  SLA  SRA  SRL  SWP
+-- 101 BIT0 BIT1 BIT2 BIT3 BIT4 BIT5 BIT6 BIT7
 -- 110 RES0 RES1 RES2 RES3 RES4 RES5 RES6 RES7
--- 111  -    -    -    -    -    -    -    -
+-- 111 SET0 SET1 SET2 SET3 SET4 SET5 SET6 SET7
+
+-- Zero flag map:
+--      000  001  010  011  100  101  110  111
+-- 000   z    z    z    z              z
+-- 001   z                             z
+-- 010   z    z    z    -
+-- 011   z         -    -
+-- 100   z    z    z    z    z    z    z    z
+-- 101  ~b   ~b   ~b   ~b   ~b   ~b   ~b   ~b
+-- 110   -    -    -    -    -    -    -    -
+-- 111   -    -    -    -    -    -    -    -
+
+-- Negative flag map:
+--      000  001  010  011  100  101  110  111
+-- 000   0    0    1    1              1
+-- 001   0                             1
+-- 010   0    0    0    1
+-- 011   -         0    0
+-- 100   0    0    0    0    0    0    0    0
+-- 101   0    0    0    0    0    0    0    0
+-- 110   -    -    -    -    -    -    -    -
+-- 111   -    -    -    -    -    -    -    -
+
+-- Half flag map:
+--      000  001  010  011  100  101  110  111
+-- 000   h    h    h    h              h
+-- 001   h                             h
+-- 010   1    0    0    1
+-- 011   0         0    0
+-- 100   0    0    0    0    0    0    0    0
+-- 101   1    1    1    1    1    1    1    1
+-- 110   -    -    -    -    -    -    -    -
+-- 111   -    -    -    -    -    -    -    -
+
+-- Carry flag map:
+--      000  001  010  011  100  101  110  111
+-- 000   c    c    c    c              c
+-- 001   -                             -
+-- 010   0    0    0    -
+-- 011   c         1    ~
+-- 100   b    b    b    b    b    b    b    0
+-- 101   -    -    -    -    -    -    -    -
+-- 110   -    -    -    -    -    -    -    -
+-- 111   -    -    -    -    -    -    -    -
 
 entity alu is
     Port (  IDATA   : in std_logic_vector(7 downto 0);          -- First operand
             ACC     : in std_logic_vector(7 downto 0);          -- Second operand
             ODATA   : out std_logic_vector(7 downto 0);         -- Result
             CE      : in std_logic;                             -- Output latch clock enable
-            CMD     : in std_logic_vector(8 downto 0);          -- CMD = X"CB"  &  CMD
+            CMD     : in std_logic_vector(5 downto 0);
             ZIN     : in std_logic;
             CIN     : in std_logic;
             HIN     : in std_logic;
@@ -58,182 +103,163 @@ architecture Behaviour of alu is
     signal niblo, nibhi : std_logic_vector(4 downto 0);         -- Nibbles of adata + muxed
     signal arith : std_logic_vector(7 downto 0);                -- concatenation of nibbles
     signal logic : std_logic_vector(7 downto 0);                -- ACC and/or/xor IDATA
+    signal shifted : std_logic_vector(7 downto 0);
 
     signal z_en : std_logic;
 
+    signal targetbit : std_logic;
+    signal zflag : std_logic;
+    signal cflag, hflag : std_logic;
+
 begin
 
+    -- Primary routing
+    process(CMD)
+        variable int : std_logic_vector(7 downto 0);
+    begin
+        with CMD(5 downto 3) select
+            int := arith when "000",
+                   arith when "001",
+                   logic when "010",      -- AND OR XOR CPL
+                   shifted when "100",    -- Shifts
+                   bitmod when "110",     -- RES
+                   bitmod when "111",     -- SET
+                   IDATA when others;     -- passthrough fallback
+
+        with int select
+            zflag <= '1' when X"00",
+                     '0' when others;
+
+        ODATA <= int;
+    end process;
+
+--  with CMD(5 downto 3) select
+--      ODATA <= arith when "000",
+--               arith when "001",
+--               logic when "010",      -- AND OR XOR CPL
+--               shifted when "100",    -- Shifts
+--               bitmod when "110",     -- RES
+--               bitmod when "111",     -- SET
+--               IDATA when others;     -- passthrough fallback
+
+    -- Arithmetic
     neg <= not IDATA + X"01";
 
-    with addsub select
+    with CMD(1) select
         muxed <= neg when '1',
                  idata when others;
 
-    with cen select
+    with CMD(0) select
         carry <= CIN when '1',
                  '0' when others;
 
-    with accmux select
-        adata <= ACC when "10",
-                 X"FF" when "01",       -- -1
-                 X"01" when "00",       -- +1
-                 X"00" when others;
+    adata <= ACC when CMD(3) = '0' else     -- + ACC
+             X"01" when CMD(2) = '0' else   -- + 1
+             X"FF";                         -- - 1
 
     niblo <= ('0' & adata(3 downto 0)) + ('0' & muxed(3 downto 0)) + (X"0" & carry);
     nibhi <= ('0' & adata(7 downto 4)) + ('0' & muxed(7 downto 4)) + (X"0" & niblo(4));
     arith <= nibhi(3 downto 0) & niblo(3 downto 0);
 
-    with CMD(7 downto 3) select
-        logic <= ACC and IDATA when "10100",                              -- AND
-                 ACC xor IDATA  when "10101",                             -- XOR
-                 ACC or IDATA when "10110",                               -- OR
-                 X"00" when others;
+    -- Logic
+    with CMD(1 downto 0) select
+        logic <= ACC and IDATA when "00",    -- AND
+                 ACC or IDATA when "01",     -- OR
+                 ACC xor IDATA  when "10",   -- XOR
+                 not ACC when others;        -- NOT
 
-    comb_proc : process(CMD)
+    -- Bit ops
+    with CMD(2 downto 0) select
+        targetbit <= IDATA(0) when "000",
+                     IDATA(1) when "001",
+                     IDATA(2) when "010",
+                     IDATA(3) when "011",
+                     IDATA(4) when "100",
+                     IDATA(5) when "101",
+                     IDATA(6) when "110",
+                     IDATA(7) when others;
+
+    with CMD(2 downto 0) select
+        shifted <= IDATA(6 downto 0) & IDATA(7) when "000",     -- RLC
+                   IDATA(6 downto 0) & CIN      when "001",     -- RL
+                   IDATA(0) & IDATA(7 downto 1) when "010",     -- RRC
+                   CIN & IDATA(7 downto 1)      when "011",     -- RR
+                   IDATA(6 downto 0) & '0'      when "100",     -- SLA
+                   IDATA(7) & IDATA(7 downto 1) when "101",     -- SRA
+                   '0' & IDATA(7 downto 1)      when "110",     -- SRL
+                   IDATA(3 downto 0) & IDATA(7 downto 4) when others;   -- SWAP
+
+    with CMD(2 downto 0) select
+        cbit <= IDATA(7) when "000",    -- RLC
+                IDATA(7) when "001",    -- RL
+                IDATA(0) when "010",    -- RRC
+                IDATA(0) when "011",    -- RR
+                IDATA(7) when "100",    -- SLA
+                IDATA(0) when "101",    -- SRA
+                IDATA(0) when "110",    -- SRL
+                '0' when others;        -- SWAP
+
+    with CMD(2 downto 0) select
+        bitmod <= IDATA(7 downto 1) & CMD(3) when "000",
+                  IDATA(7 downto 2) & CMD(3) & IDATA(0) when "001",
+                  IDATA(7 downto 3) & CMD(3) & IDATA(1 downto 0) when "010",
+                  IDATA(7 downto 4) & CMD(3) & IDATA(2 downto 0) when "011",
+                  IDATA(7 downto 5) & CMD(3) & IDATA(3 downto 0) when "100",
+                  IDATA(7 downto 6) & CMD(3) & IDATA(4 downto 0) when "101",
+                  IDATA(7) & CMD(3) & IDATA(5 downto 0) when "110",
+                  CMD(3) & IDATA(6 downto 0) when others;
+
+    -- Flags
+    with CMD(5 downto 3) select
+        ZOUT <= not targetbit when "100",
+                zflag when others;
+
+    process(CMD)
     begin
-
-        addsub <= '0';
-        accmux <= "10";
-        cen <= '0';
-
-        if CMD(8) = '0' then
-            if CMD(7 downto 6) = "00" then
-                if CMD(5 downto 0) = "101111" then  -- CPL as "2cpl(ACC) - 1"
-                    accmux <= "01";
-                    addsub <= '1';
-                else                                -- INC, DEC
-                    accmux <= CMD(1 downto 0);
-                end if;
-            elsif CMD(5 downto 4) = "00" then    -- ADD, ADC
-                cen <= cmd(3);
-            elsif CMD(5 downto 4) = "01" then -- SUB, SBC
-                addsub <= '1';
-                cen <= cmd(3);
-            elsif CMD(5 downto 3) = "111" then -- CP
-                addsub <= '1';
-            end if;
+        if CMD(5 downto 4) == "00" and CMD(2 downto 1) /= "00" then
+            NOUT <= '1';
+        elsif CMD = "010011" then
+            NOUT <= '1';
+        else
+            NOUT <= '0';
         end if;
     end process;
 
-    out_proc : process(CLK, CE, RST)
-        variable inter : std_logic_vector(7 downto 0);
+    with CMD(1) select
+        hflag <= niblo(4) when '0',
+                 not niblo(4) when others;
+
+    process(CMD)
     begin
-        if RST = '1' then
-            ODATA <= X"EE";
-        elsif rising_edge(CLK) and CE = '1' then
-            z_en <= '1';
-            ZOUT <= ZIN;
-            NOUT <= NIN;
-            HOUT <= HIN;
-            COUT <= CIN;
+        if CMD(5 downto 3) = "000" then
+            HOUT <= hflag;
+        if CMD(5 downto 3) = "101" then
+            HOUT <= '1';
+        elsif CMD = "010000" or CMD = "010011" then
+            HOUT <= '1';
+        else
+            HOUT <= '0';
+        end if;
+    end process;
 
-            if CMD(8) = '0' then
-                inter := arith;
+    with CMD(1) select
+        cflag <= nibhi(4) when '0',
+                 not nibhi(4) when others;
 
-                if addsub = '0' then
-                    COUT <= nibhi(4);
-                    HOUT <= niblo(4);
-                else
-                    COUT <= not nibhi(4);
-                    HOUT <= not niblo(4);
-                end if;
-                NOUT <= '0';
-
-                if CMD(7 downto 6) = "00" then
-                    if CMD(5 downto 0) = "101111" then  -- CPL
-                        COUT <= CIN;
-                        NOUT <= '1';
-                        HOUT <= '1';
-                        z_en <= '0';
-                    else                                -- INC, DEC
-                        COUT <= CIN;
-                        NOUT <= CMD(0);
-                    end if;
-                elsif CMD(5 downto 4) = "01" then -- SUB, SBC
-                    NOUT <= '1';
-                elsif CMD(5 downto 3) = "111" then -- CP
-                    ODATA <= ACC;
-                    NOUT <= '1';
-                elsif CMD(5 downto 4) = "10" then -- AND, XOR
-                    HOUT <= not CMD(3); -- set for AND, reset for XOR
-                    COUT <= '0';
-                    inter := logic;
-                elsif CMD(5 downto 3) = "110" then -- OR
-                    HOUT <= '0';
-                    COUT <= '0';
-                    inter := logic;
-                end if;     -- Nothing special for ADD, ADC
-
-                ODATA <= inter;
-                if z_en = '1' then
-                    if inter = X"00" then
-                        ZOUT <= '1';
-                    else
-                        ZOUT <= '0';
-                    end if;
-                end if;
-
-            else    -- CB (bitwise) operation
-                ODATA <= IDATA;
-                if CMD(7 downto 6) = "00" then -- Bit move operation
-                    NOUT <= '0';
-                    HOUT <= '0';
-                    case CMD(5 downto 3) is
-                        when "000" =>   -- RLC
-                            inter := IDATA(6 downto 0) & IDATA(7);
-                            COUT <= IDATA(7);
-                        when "010" =>   -- RL
-                            inter := IDATA(6 downto 0) & CIN;
-                            COUT <= IDATA(7);
-                        when "001" =>   -- RRC
-                            inter := IDATA(0) & IDATA(7 downto 1);
-                            COUT <= IDATA(0);
-                        when "011" =>   -- RR
-                            inter := CIN & IDATA(7 downto 1);
-                            COUT <= IDATA(0);
-                        when "100" =>   -- SLA
-                            inter := IDATA(6 downto 0) & '0';
-                            COUT <= IDATA(7);
-                        when "101" =>   -- SRA
-                            inter := IDATA(7) & IDATA(7 downto 1);
-                            COUT <= IDATA(0);
-                        when "111" =>   -- SRL
-                            inter := '0' & IDATA(7 downto 1);
-                            COUT <= IDATA(0);
-                        when "110" =>   -- SWAP
-                            inter := IDATA(3 downto 0) & IDATA(7 downto 4);
-                            COUT <= '0';
-                        when others =>
-                            inter := X"00";
-                            COUT <= '0';
-                    end case;
-                    ODATA <= inter;
-                    if z_en = '1' then
-                        if inter = X"00" then
-                            ZOUT <= '1';
-                        else
-                            ZOUT <= '0';
-                        end if;
-                    end if;
-                else    -- Single bit operation
-                    for I in 0 to 7 loop
-                        if I = CMD(5 downto 3) then
-                            case CMD(7 downto 6) is
-                                when "01" =>    -- BIT b,r
-                                    ZOUT <= not IDATA(I);
-                                    NOUT <= '0';
-                                    HOUT <= '1';
-                                when "10" =>    -- SET b,r
-                                    ODATA(I) <= '1';
-                                when "11" =>    -- RES b,r
-                                    ODATA(I) <= '0';
-                                when others =>
-                                    null;
-                            end case;
-                        end if;
-                    end loop;
-                end if;
-            end if;
+    process(CMD, cbit, CIN)
+    begin
+        if CMD(5 downto 3) = "000" then
+            COUT <= cflag;
+        elsif CMD = "011000" then
+            COUT <= cflag;
+        elsif CMD(5 downto 3) = "100" then
+            COUT <= cbit;
+        elsif CMD = "011010" then
+            COUT <= '1';
+        elsif CMD = "011011" then
+            COUT <= not CIN;
+        else
+            COUT <= '0';
         end if;
     end process;
 
